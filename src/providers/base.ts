d8f0fdf5ca9d47189bd206a9c9398019c5802d55
Sophia <me@sophia-dev.io>
2023-01-14 12:00:26 +0900

    fix(acmicpc): fix a bug that client could not fetch problem information

--- src/providers/base.ts
+++ src/providers/base.ts
@@ -32,7 +32,12 @@ export abstract class BaseProvider {
     }
 
     protected async fetch(url: string): Promise<string> {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            },
+        });
         if (!response.ok) {
             throw new Error(`Failed to fetch (${response.status}) - ${response.statusText}`);
         }
